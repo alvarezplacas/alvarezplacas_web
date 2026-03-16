e382f41df0fa43749fd6d96d490d95061384cd0b
Javier Admin <admin@alvarezplacas.com.ar>
2026-03-16 09:12:55 -0300

    chore: limpieza exhaustiva y reestructuración de archivos no esenciales

--- debug_users.js
+++
@@ -1,9 +0,0 @@
-import { query } from './src/lib/db.js';
-
-async function checkUsers() {
-    console.log('--- Checking Users Table ---');
-    const res = await query("SELECT id, email, role, full_name, assigned_seller_id FROM users");
-    console.table(res.rows);
-}
-
-checkUsers().then(() => process.exit(0));
